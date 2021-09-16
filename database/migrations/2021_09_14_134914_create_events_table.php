7aba58df465b29e882f5e84a35101c338820f540
arma <arma.malki@yahoo.com>
2021-09-16 18:23:09 +0430

    master
    Signed-off-by: arma <arma.malki@yahoo.com>

--- database/migrations/2021_09_14_134914_create_events_table.php
+++ database/migrations/2021_09_14_134914_create_events_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateGalleriesTable extends Migration
+class CreateEventsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,13 @@ class CreateGalleriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('galleries', function (Blueprint $table) {
+        Schema::create('events', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('alt')->nullable();
-            $table->string('slug')->nullable();
-            $table->longText('description')->nullable();
-
+            $table->longText('body')->nullable();
+            $table->string('slug');
             $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -33,6 +31,6 @@ class CreateGalleriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('galleries');
+        Schema::dropIfExists('events');
     }
 }
